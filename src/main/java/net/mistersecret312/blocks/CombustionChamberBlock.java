4140204f8b8eb0fc332ba313cbcfe36710a4b501
SGUDestiny <panormov2006@gmail.com>
2025-06-26 15:45:46 +0400

    Engine stuff

--- src/main/java/net/mistersecret312/blocks/CombustionChamberBlock.java
+++ src/main/java/net/mistersecret312/blocks/CombustionChamberBlock.java
@@ -17,61 +17,30 @@ import org.jetbrains.annotations.Nullable;
 public class CombustionChamberBlock extends Block
 {
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
-    public static final EnumProperty<ChamberType> TYPE = EnumProperty.create("type", ChamberType.class);
 
     public CombustionChamberBlock(Properties pProperties)
     {
         super(pProperties);
-        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH).setValue(TYPE, ChamberType.PIPELESS));
+        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
     }
 
     @Override
     public BlockState updateShape(BlockState state, Direction direction, BlockState neighbor,
                                   LevelAccessor level, BlockPos pos, BlockPos neighborPos)
     {
-        if(neighbor.getBlock() instanceof NozzleBlock nozzle)
-        {
-            if(neighbor.getValue(NozzleBlock.FACING).equals(state.getValue(FACING)))
-            {
-                if(nozzle.isVacuum())
-                    return state.setValue(TYPE, ChamberType.VACUUM);
-                else if (nozzle.isLiquidPropellant())
-                    return state.setValue(TYPE, ChamberType.ATMOSPHERE);
-                else return state.setValue(TYPE, ChamberType.PIPELESS);
-            }
-        }
         return super.updateShape(state, direction, neighbor, level, pos, neighborPos);
     }
 
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext context)
     {
-        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection()).setValue(TYPE, ChamberType.PIPELESS);
+        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection());
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
-        builder.add(FACING, TYPE);
+        builder.add(FACING);
         super.createBlockStateDefinition(builder);
-    }
-
-    public enum ChamberType implements StringRepresentable
-    {
-        VACUUM("vacuum"),
-        ATMOSPHERE("atmosphere"),
-        PIPELESS("pipeless");
-
-        String name;
-        ChamberType(String name)
-        {
-            this.name = name;
-        }
-
-        @Override
-        public String getSerializedName()
-        {
-            return this.name;
-        }
     }
 }
--- src/main/java/net/mistersecret312/blocks/NozzleBlock.java
+++ src/main/java/net/mistersecret312/blocks/NozzleBlock.java
@@ -1,19 +1,25 @@
 package net.mistersecret312.blocks;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.mistersecret312.init.BlockInit;
 import org.jetbrains.annotations.Nullable;
 
 public class NozzleBlock extends Block
 {
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
     public static final IntegerProperty HOT = IntegerProperty.create("hot", 0, 3);
+    public static final BooleanProperty ACTIVE = BooleanProperty.create("active");
+    public static final BooleanProperty HAS_PIPE = BooleanProperty.create("has_pipe");
     public boolean isVacuum;
     public boolean isLiquidPropellant;
 
@@ -23,7 +29,7 @@ public class NozzleBlock extends Block
         this.isVacuum = isVacuum;
         this.isLiquidPropellant = isLiquidPropellant;
 
-        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH).setValue(HOT, 0));
+        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH).setValue(HOT, 0).setValue(ACTIVE, false).setValue(HAS_PIPE, false));
     }
 
     public boolean isVacuum()
@@ -39,13 +45,16 @@ public class NozzleBlock extends Block
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext context)
     {
-        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection()).setValue(HOT, 0);
+        LevelAccessor accessor = context.getLevel();
+        BlockState blockState = accessor.getBlockState(context.getClickedPos().relative(context.getNearestLookingDirection()));
+        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection()).setValue(HOT, 0).setValue(ACTIVE, false)
+                .setValue(HAS_PIPE, blockState.getBlock() instanceof CombustionChamberBlock && blockState.getValue(FACING) == context.getNearestLookingDirection());
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
-        builder.add(FACING, HOT);
+        builder.add(FACING, HOT, ACTIVE, HAS_PIPE);
         super.createBlockStateDefinition(builder);
     }
 }
